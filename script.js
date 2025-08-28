c988965d53a1a6e3b14002204ad202a89bc7da3d
arab1121 <37950248+arab1121@users.noreply.github.com>
2025-08-28 22:28:13 +0300

    updated-28/8/2025

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var comments = document.getElementById("comments");
+var message = document.getElementById("message");
 var send_msg = document.getElementById("send_btn");
 var lable = document.getElementById("lable");
+var custumermsg = document.getElementById("message");
+var x = 0;
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible") {
+    console.log("The user returned to the page");
+    // write code here when return
+  } else if (document.visibilityState === "hidden") {
+    console.log("The user left the page");
+  }
+});
 //feedback massage
 send_msg.addEventListener("click", function () {
-  if (comments.value) {
-    comments.value =
-      "Thank you for your message!\nI received it and will get back to you as soon as possible.\nPlease be patient while I prepare my reply.\n\nBest regards,\nArab";
-    console.log(comments.value);
+  if (message.value && x == 0) {
+    x = x + 1;
+    message.value +=
+      "\n\n\nThank you for your message!\nI received it and will get back to you as soon as possible.\nPlease be patient while I prepare my reply.\n\nBest regards,\nArab";
+    console.log(message.value);
   }
 });
 //scrool event
